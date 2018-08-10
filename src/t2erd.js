a50d24b291b7425e7a695f2c5c55af20ac36981e
Tiago Correia <tiago.f.a.correia@gmail.com>
2018-08-10 17:03:17 +0100

    Removed image package. Separating it into another project.

--- src/t2erd.js
+++ src/t2erd.js
@@ -6,11 +6,9 @@
 const minimist = require('minimist');
 const Parser = require("./parser/parser.js");
 const Diagram = require("./drawer/diagram.js");
-const svgToImg = require("svg-to-img");
 const fs = require('fs');
 
 const argv = minimist(process.argv.slice(2));
-
 const input = argv.i;
 const output = argv.o;
 
@@ -21,11 +19,6 @@ if(!!input){
     const diagram = new Diagram(new Parser(data), {});
     const svgCode = diagram.toString();
     console.log(svgCode);
-    (async () => {
-      const image = await svgToImg.from(svgCode).toPng({
-        path: output
-      });
-    })();
   });
 }
 
